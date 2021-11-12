5d1345143665f38f32d90be188a113f79c0e7bbb
Bui Xuan Huynh <buixuanhuynha1@gmail.com>
2021-11-12 23:21:58 +0700

    useEffect to fetch data

--- react-hooks-basic/src/App.js
+++ react-hooks-basic/src/App.js
@@ -1,33 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
+import PostList from "./components/PostList";
+// import TodoForm from "./components/TodoForm";
+// import TodoList from "./components/TodoList";
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 1, title: "Eat" },
-    { id: 2, title: "Code" },
-    { id: 3, title: "Sleep" },
-    { id: 4, title: "Game" },
-  ]);
+  // const [todoList, setTodoList] = useState([
+  //   { id: 1, title: "Eat" },
+  //   { id: 2, title: "Code" },
+  //   { id: 3, title: "Sleep" },
+  //   { id: 4, title: "Game" },
+  // ]);
 
-  function handleTodoClick(todo) {
-    let indexOfItem = todoList.findIndex((item) => item.id === todo.id);
-    if (indexOfItem < 0) return;
-    let newList = [...todoList];
-    newList.splice(indexOfItem, 1);
-    setTodoList(newList);
-  }
+  const [postList, setPostList] = useState([]);
 
-  function handleTodoFormSubmit(todo) {
-    setTodoList([...todoList, { ...todo, id: todoList.length }]);
-  }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const requestUrl =
+          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+
+        const { data } = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log("Failed to fetch post list: ", error.message);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  // function handleTodoClick(todo) {
+  //   let indexOfItem = todoList.findIndex((item) => item.id === todo.id);
+  //   if (indexOfItem < 0) return;
+  //   let newList = [...todoList];
+  //   newList.splice(indexOfItem, 1);
+  //   setTodoList(newList);
+  // }
+
+  // function handleTodoFormSubmit(todo) {
+  //   setTodoList([...todoList, { ...todo, id: todoList.length }]);
+  // }
 
   return (
     <div className="app">
-      <h1> React hooks - TodoList </h1>
-      <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      <h1> React hooks - PostList </h1>
+      <PostList posts={postList} />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
     </div>
   );
 }
