d7d18eab68aa3e94e652a4def6b00cac0e6501ef
Bui Xuan Huynh <buixuanhuynha1@gmail.com>
2021-11-23 11:21:20 +0700

    set up project and redux store

---
+++ practice-redux/redux-functional-component/src/reducers/hobby.js
@@ -0,0 +1,12 @@
+const hobbyReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_HOBBY': {
+      return state;
+    }
+    case 'SET_ACTIVE_HOBBY': {
+      return state;
+    }
+    default:
+      return state;
+  }
+};
